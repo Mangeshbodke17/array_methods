7d5ca9f47db833f91a30889f774e654f6534d1b8
Mangeshbodke17 <mangeshbodke2025@gmail.com>
2025-06-22 13:12:14 +0530

    first

---
+++ script.js
@@ -0,0 +1,73 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+
+
+function PrintDeveloperbyMap() {
+  //Write your code here , just console.log
+
+  arr.map((element,i)=>{
+     if(arr[i].profession=="developer"){
+      console.log(arr[i]);
+     }
+  })
+
+
+}
+
+function PrintDeveloperbyForEach() {
+
+   arr.forEach((element,i)=>{
+
+     if(arr[i].profession=="developer"){
+      console.log(arr[i]);
+     }
+
+  })
+ 
+}
+
+function addData() {
+  //Write your code here, just console.log
+
+   let a={ id: 4, name: "rolex", age: "46", profession: "developer" };
+   
+   arr.push(a);
+   console.log(arr);
+  
+
+
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+
+
+   arr.filter((element,i)=>{
+
+ if(arr[i].profession=="developer"){
+      console.log(arr[i]);
+     }
+ 
+
+  })
+
+}
+
+function concatenateArray() {
+
+  let narr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+  let c= arr.concat(narr);
+
+  console.log(c);
+}
